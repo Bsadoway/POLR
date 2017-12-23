fbf60e5b532fd00387fcb30d1bde18f6365559f1
Blake Sadoway <blakesadoway@gmail.com>
2017-12-23 00:27:38 +0000

    added query for extending poll items

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,14 @@
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/users"
-//   }).done((users) => {
-//     for(user of users) {
-//       $("<div>").text(user.name).appendTo($("body"));
-//     }
-//   });;
-// });
+$(() => {
+  let count = 4;
+  // TODO cap item limit
+  $('.fa-plus').on('click', function(){
+    $('.new-item').append(
+      `<div class="">
+        <h4>${count}</h4>
+        <input class="p-2 d-flex" type='text' id='item${count}' name='item' placeholder="Item${count} goes here">
+        <br>
+      </div>`
+    );
+    count++;
+  });
+});

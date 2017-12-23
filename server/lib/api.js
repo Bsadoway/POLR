f368fe92fa76920954c4fddd5606bbc0f0fc06f9
Blake Sadoway <blakesadoway@gmail.com>
2017-12-23 04:38:24 +0000

    added admin page

--- server/lib/api.js
+++ server/lib/api.js
@@ -35,14 +35,14 @@ module.exports = {
       .orWhere({'polls.admin_url': url})
   },
 
-  inviteFriends: (url, friends) => {
-    // Promise.all(friends.friends.map((phoneNum) => {
-    //   return global.knex
-    //     .insert({
-    //       phone_num: phoneNum
-    //     })
-    //     .into('voters')
-    // }));
+  inviteFriends: (friends) => {
+    return Promise.all(friends.map((phoneNum) => {
+      return global.knex
+        .insert({
+          phone_num: phoneNum
+        })
+        .into('voters')
+    }));
   },
 
 

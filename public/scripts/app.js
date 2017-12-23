0264cc7200b8e2a3eb35112d516fabd359812960
Blake Sadoway <blakesadoway@gmail.com>
2017-12-23 01:50:46 +0000

    added drag and drop sortable options

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,13 +2,18 @@ $(() => {
   let count = 4;
   // TODO cap item limit
   $('.fa-plus').on('click', function(){
-    $('.new-item').append(
-      `<div class="">
-        <h4>${count}</h4>
-        <input class="p-2 d-flex" type='text' id='item${count}' name='item' placeholder="Item${count} goes here">
+    $('#sortable').append(
+      `<li class="ui-state-default">
+        <input class="p-2" type='text' id='item${count}' name='item' placeholder="Item${count} goes here">
         <br>
-      </div>`
+      </li>`
     );
     count++;
   });
+
+  $( "#sortable" ).sortable();
+  $( "#sortable" ).disableSelection();
+
+
+
 });

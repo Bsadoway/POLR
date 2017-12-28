7b552696a8a00e10816c32909c6fc70b887c4ac4
Blake Sadoway <blakesadoway@gmail.com>
2017-12-28 20:27:35 +0000

    added friends form toggle and fixed some scss

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,8 +2,8 @@ $(() => {
 
   // add an item to the vote option list
   let count = 4;
-  // TODO cap item limit
-  $('.fa-plus').on('click', function() {
+  // TODO cap item limit at 10
+  $('.add-vote-element').on('click', function() {
     $('#sortable').append(
       `<li class="ui-state-default">
         <input class="p-2" type='text' id='item${count}' name='item' placeholder="Item${count} goes here">
@@ -13,30 +13,34 @@ $(() => {
     count++;
   });
 
-
-  $('.friends-button').on('click', function() {
-    $('#friends-form').append(
-      `<form action='' method='POST'>
-         <label for='friends'>Phone #</label>
-           <input type='text' id='id1' class="p-2" name='friends'>
-           <input type='text' id='id2' class="p-2" name='friends'>
-           <button type="submit" class="btn btn-primary btn-lg btn-block"> Submit </button>
-           <br>
-           <i class="fa fa-plus fa-2x" aria-hidden="true"></i>
-       </form>`
+  // adds another sms input to the friends form
+  let smsCount = 3;
+  $('.add-sms').on('click', function() {
+    $('#friend').append(
+      `<input type='text' id='id${smsCount}' class="p-2" name='friends'>`
     );
+    smsCount++;
   });
+
+  // toggle friends sms form
+  $('.friends-button').on('click', function() {
+    $('#friends-form').toggle();
+  });
+
+
+
+
 
   //sort the options into drag and drop
   $("#sortable").sortable();
   $("#sortable").disableSelection();
 
-  $('#refresh').on('click', function () {
+  $('#refresh').on('click', function() {
     window.location.reload(true);
   });
 
   $.validate({
-    
+
   })
 
 });

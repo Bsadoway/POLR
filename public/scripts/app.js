66388382a20720c31f0db85d29bfbc357a44fa82
Blake Sadoway <blakesadoway@gmail.com>
2017-12-29 03:27:36 +0000

    fixed css for main poll form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,9 +3,9 @@ $(() => {
   // add an item to the vote option list
   let count = 4;
   // TODO cap item limit at 10
-  $('.add-vote-element').on('click', function() {
+  $('.add-option').on('click', function() {
     $('#options-box').append(
-      `<li class="ui-state-default">
+      `<li class="ui-state-default d-flex">
         <input class="p-2" type='text' id='item${count}' name='item' placeholder="option ${count}">
         <br>
       </li>`

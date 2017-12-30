62056d8d9be22ad865e7b906128f073d38398533
Frank Lam <frank.is.missing@gmail.com>
2017-12-30 08:49:22 +0000

    Changed ENV to NODE_ENV

--- knexfile.js
+++ knexfile.js
@@ -24,12 +24,11 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL + '?ssl=true',
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
-      tableName: 'migrations'
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds'
     }
   }
 
